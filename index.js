40ef3b2fe9d88f7681714a406e67b8e7600c86d0
Amal_halim <amalhalim333@gmail.com>
2023-09-04 01:41:37 +0300

    modify

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const multer = require("multer");
-const cors = require("cors");
-
-const app = express();
 const admin = require("firebase-admin");
+const cors = require("cors");
 const credenlists = require("./key.json");
 const axios = require("axios");
+
+const app = express();
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -16,7 +16,8 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
-
+//server port
+app.use(express.json());
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
@@ -32,39 +33,6 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = admin.firestore(); // Initialize Firestore here
 
-//add new user
-app.post("/addUser", async (req, res) => {
-  try {
-    const userJson = {
-      email: req.body.email,
-      name: req.body.name,
-    };
-    const response = await db.collection("users").add(userJson);
-    // console.log(response);
-    res.send(response);
-  } catch (error) {
-    res.send(error);
-  }
-});
-//get All User
-app.get("/user", async (req, res) => {
-  try {
-    console.log(req.body);
-    const usersRef = db.collection("users");
-    const response = await usersRef.get();
-    // console.log(response);
-    let responsArr = [];
-    response.forEach(doc => {
-      responsArr.push();
-      responsArr.push(doc.data());
-    });
-
-    res.send(responsArr);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
 //update
 app.post("/update", async (req, res) => {
   try {
@@ -78,7 +46,6 @@ app.post("/update", async (req, res) => {
   }
 });
 //delete
-
 app.delete("/delete", async (req, res) => {
   try {
     const response = await db.collection("users").doc(req.params.id).delete();
@@ -88,24 +55,7 @@ app.delete("/delete", async (req, res) => {
   }
 });
 
-// API for shorting link
-app.post("/Shortening", async (req, res) => {
-  try {
-    const LongUrl = ` https://api.shrtco.de/v2/shorten?url=${req.query.url}/u/0/project/thenodeproject-1dc6c/firestore/data/~2Fimages~2FT4aSvZ7w4ne5yCHvYqDx/very/long/link.html`;
-    // Make an Axios request to the shortening service
-    const axiosresponse = await axios.post(LongUrl);
-    // Save the shortened URL in the database
-    const new_short_link = axiosresponse.data.result.full_short_link;
-    const response = await db.collection("Urls").add({ new_short_link });
-    res.send(new_short_link);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-// Get a reference to the storage service, which is used to create references in your storage bucket
-
-//add new user
+//add new file
 app.post("/addfile", upload.single("image"), async (req, res) => {
   try {
     const image = req.file;
@@ -126,40 +76,46 @@ app.put("/updateFiles/:id", upload.single("image"), async (req, res) => {
   try {
     const fileId = req.params.id;
     console.log("fileId", fileId);
-    
+    // Assuming the uploaded file is stored
     const updatedFileData = {
-      image: req.file.path, // Assuming the uploaded file is stored in the "path" property of the "req.file" object
-    }; console.log("updatedFileData", updatedFileData);
-    
-        const fileRef = db.collection("AllFiles").doc(fileId);
-        const fileSnapshot = await fileRef.get();
+      name: req.file.originalname,
+      image: req.file.path,
+    };
+    console.log("updatedFileData", updatedFileData);
 
-        if (!fileSnapshot.exists) {
-          return res.status(404).send("File not found");
-        }
-      await fileRef.update(updatedFileData);
-      res.status(200).send("File updated successfully");
+    const fileRef = db.collection("AllFiles").doc(fileId);
+    // const fileSnapshot = await fileRef.exists();
+
+    // if (!fileSnapshot.exists) {
+    //   return res.status(404).send("File not found");
+    // }
+    await fileRef.update(updatedFileData);
+    res.status(200).send("File updated successfully");
   } catch (error) {
     res.send(error);
     console.log(error);
   }
 });
-app.delete("/deletefile/:id", async (req, res) => {
+//delete file
+app.delete("/deletefile", async (req, res) => {
   try {
-    const id = req.params.id;
+    const id = req.body.id;
     // console.log(id);
     const fileRef = db.collection("AllFiles").doc(id);
     const file = await fileRef.get();
-      console.log(file);
+    // console.log(file);
     if (!file.exists) {
       res.status(404).send("File not found");
       return;
     }
     // Delete the file from storage
     const imageURL = file.data().imageURL;
-    await admin.storage().bucket().file(imageURL).delete();
+    const bucket = admin.storage().bucket();
+    await bucket.file(imageURL).delete();
+    console.log("imageURL", imageURL);
     // Delete the file document from Firestore
     await fileRef.delete();
+    res.status(500).send("done");
   } catch (error) {
     res.status(500).send(error);
   }
@@ -182,3 +138,31 @@ app.get("/getAllFiles", async (req, res) => {
   }
 });
 
+//get file
+app.get("/getSingleFile", async (req, res) => {
+  const fileId = req.body.id;
+  const fileRef = db.collection("AllFiles").doc(fileId);
+  const fileSnapshot = await fileRef.get();
+
+  if (!fileSnapshot.exists) {
+    return res.status(404).send("File not found");
+  }
+
+  const file = fileSnapshot.data();
+  res.send(file);
+});
+
+// API for shorting link
+app.post("/ShorteningURL", async (req, res) => {
+  try {
+    const LongUrl = ` https://api.shrtco.de/v2/shorten?url=${req.query.url}/u/0/project/thenodeproject-1dc6c/firestore/data/~2Fimages~2FT4aSvZ7w4ne5yCHvYqDx/very/long/link.html`;
+    // Make an Axios request to the shortening service
+    const axiosresponse = await axios.post(LongUrl);
+    // Save the shortened URL in the database
+    const new_short_link = axiosresponse.data.result.full_short_link;
+    const response = await db.collection("Urls").add({ new_short_link });
+    res.send(new_short_link);
+  } catch (error) {
+    res.send(error);
+  }
+});
